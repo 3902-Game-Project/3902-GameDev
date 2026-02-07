1224bdc9634b9a758e56ce2d0ad9fbf723539d8e
Cuetron <Quintin@amonhaus.com>
2026-02-07 08:15:10 -0800

    Added Structure for the new sprite adding

---
+++ GameProject/Interfaces/IEnemy.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace sprint0
+{
+    public interface IEnemy
+    {
+        void Update();
+        void Draw(SpriteBatch spriteBatch);
+        void ChangeDirection();
+        void TakeDamage();
+    }
+}
