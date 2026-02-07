9d10de8d66331c11e6086d2fae3621884af736fd
Cuetron <Quintin@amonhaus.com>
2026-02-07 08:32:59 -0800

    Fixed implementation of the SnakeSprite and snakeSpritesheet

--- GameProject/Factories/EnemySpriteFactory.cs
+++ GameProject/Factories/EnemySpriteFactory.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using GameProject.Interfaces;
+using GameProject.Sprites;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using sprint0;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +13,27 @@ namespace GameProject.Factories
 {
     internal class EnemySpriteFactory
     {
+        private Texture2D snakeSpritesheet;
+        //...
+
+        private static EnemySpriteFactory instance = new EnemySpriteFactory();
+
+        public static EnemySpriteFactory Instance
+        {
+            get { return instance; }
+        }
+
+        private EnemySpriteFactory() { }
+
+        public void LoadAllTextures(ContentManager content)
+        {
+            snakeSpritesheet = content.Load<Texture2D>("snakeSpritesheet.png");
+            //...
+        }
+        
+        public IEnemy CreateSnakeSprite()
+        {
+            return new SnakeSprite(snakeSpritesheet, 32, 32);
+        }
     }
 }
--- GameProject/Sprites/SnakeSprite.cs
+++ GameProject/Sprites/SnakeSprite.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using sprint0;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,26 @@ using System.Threading.Tasks;
 
 namespace GameProject.Sprites
 {
-    internal class SnakeSprite
+    internal class SnakeSprite(Texture2D snakeSpritesheet, int len, int width) : IEnemy
     {
+        public void ChangeDirection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TakeDamage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
