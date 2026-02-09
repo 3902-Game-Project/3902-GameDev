47fd9c8ffaf4385421d5507fc11515639e1705bb
santosh-sch <258964949+santosh-sch@users.noreply.github.com>
2026-02-08 21:47:49 -0500

    start tweaking editorconfig

--- Game1.cs
+++ Game1.cs
@@ -4,80 +4,72 @@
  * Santosh Schaefer
  */
 
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using GameProject.Controllers;
 using GameProject.Interfaces;
 using GameProject.Sprites;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace GameProject;
 
-public class Game1 : Game
-{
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
+public class Game1 : Game {
+  private GraphicsDeviceManager _graphics;
+  private SpriteBatch _spriteBatch;
 
-    private Texture2D myTexture;
-    public Texture2D Texture
-    {
-        get { return myTexture; }
-    }
-    private SpriteFont myFont;
-    private IController keyboardController;
-    private ISprite currentSprite;
-    public ISprite CurrentSprite
-    {
-        set { currentSprite = value; }
-    }
+  private Texture2D myTexture;
+  public Texture2D Texture {
+    get { return myTexture; }
+  }
+  private SpriteFont myFont;
+  private IController keyboardController;
+  private ISprite currentSprite;
+  public ISprite CurrentSprite {
+    set { currentSprite = value; }
+  }
 
-    public Game1()
-    {
+  public Game1() {
 
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
+    _graphics = new GraphicsDeviceManager(this);
+    Content.RootDirectory = "Content";
+    IsMouseVisible = true;
 
-    }
-    protected override void Initialize()
-    {
-        keyboardController = new KeyboardController(this);
+  }
+  protected override void Initialize() {
+    keyboardController = new KeyboardController(this);
 
 
-        base.Initialize();
-    }
+    base.Initialize();
+  }
 
-    protected override void LoadContent()
-    {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
+  protected override void LoadContent() {
+    _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        myTexture = Content.Load<Texture2D>("Metro");
-        currentSprite = new FixedSprite(myTexture, new Vector2(400, 200));
+    myTexture = Content.Load<Texture2D>("Metro");
+    currentSprite = new FixedSprite(myTexture, new Vector2(400, 200));
 
-        myFont = Content.Load<SpriteFont>("CreditsFont");
-    }
-    
+    myFont = Content.Load<SpriteFont>("CreditsFont");
+  }
 
-    protected override void Update(GameTime gameTime)
-    {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
 
-        keyboardController.Update(gameTime);
-        currentSprite.Update(gameTime);
+  protected override void Update(GameTime gameTime) {
+    if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+      Exit();
 
-        base.Update(gameTime);
-    }
+    keyboardController.Update(gameTime);
+    currentSprite.Update(gameTime);
 
-    protected override void Draw(GameTime gameTime)
-    {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+    base.Update(gameTime);
+  }
 
-        _spriteBatch.Begin();
-        currentSprite.Draw(_spriteBatch);
-        _spriteBatch.End();
+  protected override void Draw(GameTime gameTime) {
+    GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        base.Draw(gameTime);
-    }
+    _spriteBatch.Begin();
+    currentSprite.Draw(_spriteBatch);
+    _spriteBatch.End();
+
+    base.Draw(gameTime);
+  }
 
 }
