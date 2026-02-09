cf7dd07620a3b97f87c8e151ef75809fb029e629
Cuetron <Quintin@amonhaus.com>
2026-02-08 21:35:13 -0800

    remake branch due to a NET error with temp files and Git.  Fix implementation parameters for IEnemy

--- Interfaces/IEnemy.cs
+++ Interfaces/IEnemy.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
-namespace GameProject.Interfaces;
-
-public interface IEnemy {
-  void Update();
-  void Draw(SpriteBatch spriteBatch);
-  void ChangeDirection();
-  void TakeDamage();
+namespace GameProject.Interfaces
+{
+  public interface IEnemy {
+    void Update(GameTime gameTime); 
+    void Draw(SpriteBatch spriteBatch);
+    void ChangeDirection();
+    void TakeDamage();
+  }
 }
--- Sprites/SnakeSprite.cs
+++ Sprites/SnakeSprite.cs
@@ -1,23 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 using GameProject.Interfaces;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace GameProject.Sprites;
+namespace GameProject.Sprites {
+  public class SnakeSprite : IEnemy {
+    private Texture2D texture;
+    private Vector2 position;
+    private Vector2 startPosition;
+    private List<Rectangle> sourceRectangles;
 
-internal class SnakeSprite(Texture2D snakeSpritesheet, int len, int width) : IEnemy {
-  public void ChangeDirection() {
-    throw new NotImplementedException();
-  }
+    private int direction = -1;
+    private int speed = 2;
+    private int patrolDistance = 100;
 
-  public void Draw(SpriteBatch spriteBatch) {
-    throw new NotImplementedException();
-  }
+    private int currentFrame;
+    private double timer;
+    private double frameInterval = 0.2;
 
-  public void TakeDamage() {
-    throw new NotImplementedException();
-  }
+    public SnakeSprite(Texture2D texture, Vector2 startPosition) {
+      this.texture = texture;
+      this.position = startPosition;
+      this.startPosition = startPosition;
+      this.timer = 0;
+      this.currentFrame = 0;
 
-  public void Update() {
-    throw new NotImplementedException();
+      sourceRectangles = new List<Rectangle>();
+      sourceRectangles.Add(new Rectangle(0, 0, 16, 16));
+      sourceRectangles.Add(new Rectangle(16, 0, 16, 16)); 
+    }
+
+    public void Update(GameTime gameTime) {
+      timer += gameTime.ElapsedGameTime.TotalSeconds;
+      if (timer >= frameInterval) {
+        currentFrame++;
+        if (currentFrame >= sourceRectangles.Count)
+          currentFrame = 0;
+        timer = 0;
+      }
+
+      position.X += speed * direction;
+
+      if (direction == -1 && position.X <= startPosition.X - patrolDistance) {
+        direction = 1; 
+      } else if (direction == 1 && position.X >= startPosition.X + patrolDistance) {
+        direction = -1; 
+      }
+    }
+
+    public void Draw(SpriteBatch spriteBatch) {
+      Rectangle sourceRectangle = sourceRectangles[currentFrame];
+      SpriteEffects effects = (direction == -1) ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+
+      spriteBatch.Draw(texture, position, sourceRectangle, Color.White, 0f, Vector2.Zero, 2.0f, effects, 0f);
+    }
+
+    public void ChangeDirection() { /* TODO */ }
+    public void TakeDamage() { /* TODO */ }
   }
 }
