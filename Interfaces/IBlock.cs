7fee730510161b21e32d5b2eed73cbfc60e4791a
Aaron Vanderbilt <vanderbilt.15@buckeyemail.osu.edu>
2026-02-11 17:54:50 -0500

    added Blocks folder to project, added IBlock to Interfaces, added start of block atlas to the Content editor

---
+++ Interfaces/IBlock.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameProject.Interfaces;
+
+public interface IBlock {
+  public void Update(GameTime gameTime);
+  public void Draw(SpriteBatch spriteBatch);
+}
