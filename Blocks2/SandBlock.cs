0e66febd211d2dea77f76fed9d5eff8c0870a0b2
Aaron Vanderbilt <vanderbilt.15@buckeyemail.osu.edu>
2026-02-14 11:05:06 -0500

    updated SandBlock,

--- Blocks2/SandBlock.cs
+++ Blocks2/SandBlock.cs
@@ -9,15 +9,24 @@ namespace GameProject.Blocks2;
 public class SandBlock : IBlock{
   private static Texture2D texture;
   private Rectangle sourceRect;
+  public int xPos { get; private set; }
+  public int yPos { get; private set; }
 
-  public SandBlock() {
 
+
+  public SandBlock(Texture2D sandTexture, int x, int y) {
+    texture = sandTexture;
+    xPos = x;
+    yPos = y;
+    sourceRect = new Rectangle(0, 0, 63, 63); // will be in xml (or something else) file later -Aaron
   }
   public void Update(GameTime gameTime) {
     // not needed
   }
 
   public void Draw(SpriteBatch spriteBatch) {
-    spriteBatch.Draw(texture, new Vector2(300, 200), sourceRect, Color.White, 0.0f, new Vector2(0, 0), 2.0f, SpriteEffects.None, 0.0f);
+    spriteBatch.Draw(texture, new Vector2(xPos, yPos), sourceRect, 
+                      Color.White, 0.0f, new Vector2(0, 0), 2.0f, 
+                      SpriteEffects.None, 0.0f);
   }
 }
