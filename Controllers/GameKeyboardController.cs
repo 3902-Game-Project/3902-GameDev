2e39d0d902d46f56d59b6b049b00a9c46cf5b719
emmettluzar <emmettluzar@gmail.com>
2026-02-14 17:21:30 -0500

    Mapped Z and N keys to PlayerUseItemCommand

--- Controllers/GameKeyboardController.cs
+++ Controllers/GameKeyboardController.cs
@@ -16,7 +16,9 @@ public class GameKeyboardController(Game1 game) : IController {
     {Keys.D4, new LeftAndRightAnimatedCommand(game)},
     {Keys.O, new EnemySnakeCommand(game)},
     {Keys.Y, new NextBlockCommand(game)},
-    {Keys.T, new PreviousBlockCommand(game)}
+    {Keys.T, new PreviousBlockCommand(game)},
+    {Keys.Z, new PlayerUseItemCommand(game)},
+    {Keys.N, new PlayerUseItemCommand(game)}
   };
 
   public void Update(GameTime gameTime) {
