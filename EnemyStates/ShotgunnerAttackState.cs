0f6580d33f899ee81ac14b0f5d8809e32b59d87c
Cuetron <Quintin@amonhaus.com>
2026-02-16 09:39:03 -0800

    Implemented working shotgunner enemy.  animation still WIP

---
+++ EnemyStates/ShotgunnerAttackState.cs
@@ -0,0 +1,46 @@
+﻿using GameProject.Sprites;
+using GameProject.Interfaces;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace GameProject.States {
+  public class ShotgunnerAttackState : IShotgunnerState {
+    private ShotgunnerSprite shotgunner;
+    private double timer;
+    private double animationTimer;
+
+    public ShotgunnerAttackState(ShotgunnerSprite shotgunner) {
+      this.shotgunner = shotgunner;
+
+      // Lunge in the direction we are currently looking
+      this.shotgunner.Velocity = new Vector2(shotgunner.FacingDirection * 0, 0);
+
+      this.shotgunner.CurrentSourceRectangles = new List<Rectangle>
+      {
+                new Rectangle(9, 142, 13, 16),
+            };
+      this.shotgunner.CurrentFrame = 0;
+    }
+
+    public void Update(GameTime gameTime) {
+      float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+      timer += dt;
+
+      animationTimer += dt;
+      if (animationTimer >= 0.05) {
+        shotgunner.CurrentFrame++;
+        if (shotgunner.CurrentFrame >= shotgunner.CurrentSourceRectangles.Count)
+          shotgunner.CurrentFrame = 0;
+        animationTimer = 0;
+      }
+
+      // 2. Movement
+      shotgunner.Position += shotgunner.Velocity * dt;
+
+      // 3. Return to Idle
+      if (timer > 0.5) {
+        shotgunner.ChangeState(new ShotgunnerIdleState(shotgunner));
+      }
+    }
+  }
+}
---
+++ Sprites/ShotgunnerSprite.cs
@@ -0,0 +1,53 @@
+﻿using GameProject.Interfaces;
+using GameProject.States;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace GameProject.Sprites {
+  public class ShotgunnerSprite : IEnemy {
+    // Data needed by states
+    public Texture2D Texture { get; private set; }
+    public Vector2 Position;
+    public Vector2 Velocity;
+    public int FacingDirection = 1;
+
+    public List<Rectangle> CurrentSourceRectangles;
+    public int CurrentFrame;
+
+    private IShotgunnerState state;
+
+    public ShotgunnerSprite(Texture2D texture, Vector2 position) {
+      Texture = texture;
+      Position = position;
+      state = new ShotgunnerWanderState(this);
+    }
+
+    public void ChangeState(IShotgunnerState newState) {
+      state = newState;
+    }
+
+    public void Update(GameTime gameTime) {
+      state.Update(gameTime);
+
+      // Keep inside bounds
+      if (Position.X < 0) Position.X = 0;
+    }
+
+    public void Draw(SpriteBatch spriteBatch) {
+      if (CurrentSourceRectangles == null || CurrentSourceRectangles.Count == 0) return;
+
+      Rectangle source = CurrentSourceRectangles[CurrentFrame];
+
+      // Flip logic
+      SpriteEffects effect = (FacingDirection > 0) ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+
+      // Origin at feet
+      Vector2 origin = new Vector2(source.Width / 2, source.Height);
+
+      spriteBatch.Draw(Texture, Position, source, Color.White, 0f, origin, 2f, effect, 0f);
+    }
+
+    public void TakeDamage() { /* ... */ }
+  }
+}
