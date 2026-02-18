67caa383ab84e14d18843698ce88a57b1105d197
li14473 <li.14473@osu.edu>
2026-02-18 13:52:16 -0500

    add player sprite sheet

--- Globals/Textures.cs
+++ Globals/Textures.cs
@@ -6,11 +6,13 @@ namespace GameProject.Globals;
 public class TextureStore(Game1 game) : IGlobalData {
   public Texture2D MetroTexture { get; private set; }
   public Texture2D BlockTextures { get; private set; }
+  public Texture2D PlayerTexture { get; private set; }
 
   public void Initialize() { }
 
   public void LoadContent() {
     MetroTexture = game.Content.Load<Texture2D>("Metro"); // is this needed? -Aaron
     BlockTextures = game.Content.Load<Texture2D>("desert-atlas-v1");
+    PlayerTexture = game.Content.Load<Texture2D>("playerSpritesheet");
   }
 }
--- PlayerStates/PlayerStaticState.cs
+++ PlayerStates/PlayerStaticState.cs
@@ -6,32 +6,48 @@ namespace GameProject.PlayerStates {
   public class PlayerStaticState : IPlayerState {
     private Player player;
 
+    // Same rectangles as moving state
+    private Rectangle SpriteRight = new Rectangle(766, 62, 213, 316);
+    private Rectangle SpriteLeft = new Rectangle(1528, 425, 180, 319);
+
     public PlayerStaticState(Player player) {
       this.player = player;
     }
 
     public void Update(GameTime gameTime) {
+      // If we start moving, switch state
+      if (player.Velocity != Vector2.Zero) {
+        player.State = new PlayerAnimatedMovingState(player);
+      }
     }
 
-    public void UseItem() {
-    }
+    public void UseItem() { }
 
     public void Draw(SpriteBatch spriteBatch) {
-      Texture2D texture = player.game.GlobalVars.Assets.Textures.MetroTexture;
-      Rectangle sourceRect = new Rectangle(65, 2, 26, 24);
+      Texture2D texture = player.game.GlobalVars.Assets.Textures.PlayerTexture;
 
-      Vector2 origin = new Vector2(sourceRect.Width / 2, sourceRect.Height / 2);
+      Rectangle sourceRect;
+      Vector2 origin;
+
+      // Check direction even when standing still
+      if (player.Direction == FacingDirection.Right) {
+        sourceRect = SpriteRight;
+      } else {
+        sourceRect = SpriteLeft;
+      }
+
+      origin = new Vector2(sourceRect.Width / 2 , sourceRect.Height / 2 );
 
       spriteBatch.Draw(
-          texture: texture,
-          position: player.Position,
-          sourceRectangle: sourceRect,
-          color: Color.White,
-          rotation: 0f,
-          origin: origin,
-          scale: 2f,
-          effects: SpriteEffects.None,
-          layerDepth: 0f
+          texture,
+          player.Position,
+          sourceRect,
+          Color.White,
+          0f,
+          origin,
+          1f,
+          SpriteEffects.None,
+          0f
       );
     }
   }
