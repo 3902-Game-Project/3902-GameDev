d5de4d46b4bb9f3213c338c7fbdb3640dfa0533d
JJWeixel <weixeljohn@gmail.com>
2026-02-22 15:51:15 -0500

    Altered BombProjectile class

--- Projectiles/BombProjectile.cs
+++ Projectiles/BombProjectile.cs
@@ -6,46 +6,54 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace GameProject.Projectiles;
 
-public class BombProjectile : IProjectile {
-  private Texture2D texture;
-  private Vector2 position;
-  private List<Rectangle> sourceRectangles;
-  private Rectangle currentSourceRect;
-  private Animation bombAnimation;
-  private Vector2 origin;
-  public bool IsExpired { get; private set; } = false;
+public class BombProjectile : IProjectile
+{
+    private Texture2D texture;
+    private Vector2 position;
+    private List<Rectangle> sourceRectangles;
+    private Rectangle currentSourceRect;
+    private Animation bombAnimation;
+    private Vector2 origin;
+    public bool IsExpired { get; private set; } = false;
 
-  public void Draw(SpriteBatch spriteBatch) {
-    origin = new Vector2(currentSourceRect.Width / 2, currentSourceRect.Height / 2);
+    private Vector2 direction;
+    private float velocity;
+    private float lifetime;
+    private float lifetimeCounter;
 
-    spriteBatch.Draw(
-      texture,
-      position,
-      currentSourceRect,
-      Color.White,
-      0f,
-      origin,
-      1f,
-      SpriteEffects.None,
-      0f
-    );
-  }
 
-  public void Update(GameTime gameTime) {
-    bombAnimation.Update(gameTime);
-    currentSourceRect = bombAnimation.CurrentFrame;
-  }
+  public void Draw(SpriteBatch spriteBatch)
+    {
+        origin = new Vector2(currentSourceRect.Width / 2, currentSourceRect.Height / 2);
 
-  public void Instantiate(Vector2 startPosition, Vector2 direction, float velocity, float lifetime) {
-    // Logic for instantiating the bomb projectile
-    this.position = startPosition;
-    this.IsExpired = false;
-  }
+        spriteBatch.Draw(
+            texture,
+            position,
+            currentSourceRect,
+            Color.White,
+            0f,
+            origin,
+            1f,
+            SpriteEffects.None,
+            0f
+        );
+    }
 
-  public BombProjectile(Texture2D texture, Vector2 startPosition) {
-    this.texture = texture;
-    this.position = startPosition;
-    bombAnimation = new Animation(sourceRectangles, 5);
-    currentSourceRect = bombAnimation.CurrentFrame;
-  }
+    public void Update(GameTime gameTime)
+    {
+        bombAnimation.Update(gameTime);
+        currentSourceRect = bombAnimation.CurrentFrame;
+    }
+
+    public BombProjectile(Texture2D texture, Vector2 startPosition, Vector2 direction, float velocity, float lifetime)
+    {
+        this.texture = texture;
+        position = startPosition;
+        this.direction = direction;
+        this.velocity = velocity;
+        this.lifetime = lifetime;
+        lifetimeCounter = 0f;
+        bombAnimation = new Animation(sourceRectangles, 5);
+        currentSourceRect = bombAnimation.CurrentFrame;
+    }
 }
