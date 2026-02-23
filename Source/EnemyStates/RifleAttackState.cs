aad28db8932d37cd8376ef26939e0de090c51c7e
Cuetron <Quintin@amonhaus.com>
2026-02-22 18:03:20 -0800

    added rifle enemy

---
+++ Source/EnemyStates/RifleAttackState.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using GameProject.Interfaces;
+using GameProject.Sprites;
+using Microsoft.Xna.Framework;
+
+namespace GameProject.States;
+
+public class RifleAttackState : IRifleState {
+  private RifleSprite rifle;
+  private double stateTimer;
+  private double animationTimer;
+  private double timePerFrame = 0.15;
+
+  public RifleAttackState(RifleSprite rifle) {
+    this.rifle = rifle;
+
+    this.rifle.Velocity = Vector2.Zero;
+
+    this.rifle.CurrentSourceRectangles = new List<Rectangle> {
+      new(198, 91, 21, 27),
+      new(260, 91, 22, 27),
+      new(323, 89, 23, 29),
+    };
+    this.rifle.CurrentFrame = 0;
+  }
+
+  public void Update(GameTime gameTime) {
+    float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+    animationTimer += dt;
+    if (animationTimer >= timePerFrame) {
+      if (rifle.CurrentFrame < rifle.CurrentSourceRectangles.Count - 1) {
+        rifle.CurrentFrame++;
+        animationTimer = 0;
+      }
+    }
+    stateTimer += dt;
+    if (stateTimer > 1.0) {
+      rifle.ChangeState(new RifleIdleState(rifle));
+    }
+  }
+}
---
+++ Source/EnemyStates/RifleIdleState.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using GameProject.Interfaces;
+using GameProject.Sprites;
+using Microsoft.Xna.Framework;
+
+namespace GameProject.States;
+
+public class RifleIdleState : IRifleState {
+  private RifleSprite rifle;
+  private double timer;
+  private double animationTimer;
+  private System.Random random;
+
+  public RifleIdleState(RifleSprite rifle) {
+    this.rifle = rifle;
+    this.random = new System.Random();
+
+    this.rifle.Velocity = Vector2.Zero;
+
+    this.rifle.CurrentSourceRectangles = new List<Rectangle> {
+      new(71, 130, 23, 28),
+      new(134, 130, 23, 28),
+      new(196, 130, 23, 28),
+      new(259, 130, 23, 28),
+    };
+    this.rifle.CurrentFrame = 0;
+  }
+
+  public void Update(GameTime gameTime) {
+    animationTimer += gameTime.ElapsedGameTime.TotalSeconds;
+    if (animationTimer > 0.2) {
+      rifle.CurrentFrame++;
+      if (rifle.CurrentFrame >= rifle.CurrentSourceRectangles.Count) {
+        rifle.CurrentFrame = 0;
+      }
+      animationTimer = 0;
+    }
+    timer += gameTime.ElapsedGameTime.TotalSeconds;
+
+    if (timer > 1.0) {
+      int choice = random.Next(0, 2);
+      if (choice == 0) {
+        rifle.ChangeState(new RifleAttackState(rifle));
+      } else {
+        rifle.ChangeState(new RifleWanderState(rifle));
+      }
+    }
+  }
+}
