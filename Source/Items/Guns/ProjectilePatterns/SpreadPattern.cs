37986d9a654942aaba7db473f75ac45fb411b9f2
JJWeixel <weixeljohn@gmail.com>
2026-03-01 16:49:57 -0500

    Added SpreadPattern

---
+++ Source/Items/Guns/ProjectilePatterns/SpreadPattern.cs
@@ -0,0 +1,17 @@
+using GameProject.Interfaces;
+using GameProject.Managers;
+using Microsoft.Xna.Framework;
+using GameProject.Factories;
+using System;
+
+namespace GameProject.Items;
+
+public class SpreadPattern : IProjectilePattern {
+  public void SpawnProjectiles(ProjectileManager projectileManager, Vector2 spawnPosition, Vector2 direction, GunStats stats) {
+    for (int i = 0; i < stats.PelletCount; i++) {
+      float angle = -stats.SpreadAngle / 2 + stats.SpreadAngle / (stats.PelletCount - 1) * i;
+      Vector2 rotatedDirection = Vector2.Transform(direction, Matrix.CreateRotationZ(MathHelper.ToRadians(angle)));
+      projectileManager.AddProjectile(ProjectileFactory.Instance.CreateBullet(spawnPosition, rotatedDirection, stats.BulletVelocity, stats.BulletLifetime));
+    }
+  }
+}
