42c296af6b28182f2805af1a325531e98783124c
emmettluzar <emmettluzar@gmail.com>
2026-03-06 12:58:46 -0500

    Updated ILevelManager

--- Source/Interfaces/ILevelManager.cs
+++ Source/Interfaces/ILevelManager.cs
@@ -1,6 +1,8 @@
 ﻿namespace GameProject.Interfaces;
 
 public interface ILevelManager : IInitable, IUpdatable, IGPDrawable {
+  ILevel CurrentLevel { get; }
+
   public void SwitchLevel(int newLevelIndex);
   public void PreviousLevel();
   public void NextLevel();
