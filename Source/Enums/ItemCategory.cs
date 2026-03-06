9e1d264c4c1c13d3f82819ab370a7b380fc6ecc3
emmettluzar <emmettluzar@gmail.com>
2026-03-06 11:55:11 -0500

    Created Source/Enums and added ItemCategory class

---
+++ Source/Enums/ItemCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject.Enums {
+  public enum ItemCategory {
+    Sidearm,     // Small guns (Revolver)
+    Primary,     // Big guns (Rifle, Shotgun)
+    Consumable   // Whiskey, Bombs, etc
+  }
+}
