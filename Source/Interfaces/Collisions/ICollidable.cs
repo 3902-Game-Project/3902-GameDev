ec43e74e52f426155bfa1c77e13f48d2e4b521f7
JJWeixel <weixeljohn@gmail.com>
2026-03-06 14:27:12 -0500

    Added ICollidable

---
+++ Source/Interfaces/Collisions/ICollidable.cs
@@ -0,0 +1,14 @@
+using Microsoft.Xna.Framework;
+
+namespace GameProject.Interfaces;
+
+public enum Layer { Player, Environment, Enemies, Projectiles }
+
+public interface ICollidable {
+  IShape Shape { get; }
+  Vector2 Position { get; }
+  Layer Layer { get; }
+  Layer Mask { get; }
+
+  void OnCollision(ICollidable other);
+}
