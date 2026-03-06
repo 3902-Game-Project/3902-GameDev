f5436ed95757b64fa41525947f651d66e4f747b7
Eric <li.14473@osu.edu>
2026-03-05 22:46:41 -0500

    Add collision sides

---
+++ Source/CollisionResponse/CollisionSides.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject.Source.CollisionResponse;
+public enum CollisionSide {
+  Top,
+  Bottom,
+  Left,
+  Right,
+  None
+}
