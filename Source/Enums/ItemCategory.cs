af300bfb2628bc8dbaa7ae7e7dd6a15e00bf6ac9
emmettluzar <emmettluzar@gmail.com>
2026-03-06 12:18:54 -0500

    Added whip to item categories

--- Source/Enums/ItemCategory.cs
+++ Source/Enums/ItemCategory.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace GameProject.Enums {
   public enum ItemCategory {
+    Melee,       // Whip, machete maybe, etc.
     Sidearm,     // Small guns (Revolver)
     Primary,     // Big guns (Rifle, Shotgun)
     Consumable   // Whiskey, Bombs, etc
