1e9237aa4e0f9fd67c6547ee5910abe809c3be09
santosh-sch <258964949+santosh-sch@users.noreply.github.com>
2026-03-08 23:30:48 -0400

    implement parsing for all known blocks

--- Source/Factories/BlockSpriteFactory.cs
+++ Source/Factories/BlockSpriteFactory.cs
@@ -32,11 +32,20 @@ public class BlockSpriteFactory {
   public IBlock CreateSandBlockSprite(float x, float y) {
     return new SandBlock(blockTextures, new Vector2(x, y));
   }
+  public IBlock CreateRedSandBlockSprite(float x, float y) {
+    return new RedSandBlock(blockTextures, new Vector2(x, y));
+  }
   public IBlock CreateWoodPlankBlockSprite(float x, float y) {
     return new WoodPlankBlock(blockTextures, new Vector2(x, y));
   }
 
   /* Wall Blocks */
+  public IBlock CreateLogBlockSprite(float x, float y) {
+    return new LogBlock(blockTextures, new Vector2(x, y));
+  }
+  public IBlock CreateLogCornerBlockSprite(float x, float y) {
+    return new LogCornerBlock(blockTextures, new Vector2(x, y));
+  }
   public IBlock CreateRockBlockSprite(float x, float y) {
     return new RockBlock(blockTextures, new Vector2(x, y));
   }
