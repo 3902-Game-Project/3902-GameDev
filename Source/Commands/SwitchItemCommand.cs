c243fd8e9605d069041f8aff663925eb7260ca38
emmettluzar <emmettluzar@gmail.com>
2026-04-02 18:06:45 -0400

    Deleted SwitchItemCommand

--- Source/Commands/SwitchItemCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using GameProject.Enums;
-using GameProject.Interfaces;
-
-namespace GameProject.Commands;
-public class SwitchItemCommand(Player player, ItemCategory category) : ICommand {
-  public void Execute() {
-    player.Inventory.SwitchActiveItem(category);
-  }
-}
