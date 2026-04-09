2f56b96282ad9985eea1f3b156c1b30028995587
santosh-sch <258964949+santosh-sch@users.noreply.github.com>
2026-04-09 19:32:30 -0400

    use primary constructor

--- Source/Items/Utility/KeyItem.cs
+++ Source/Items/Utility/KeyItem.cs
@@ -7,32 +7,21 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace GameProject.Source.Items.Utility;
 
-public class KeyItem : IItem, IWorldPickup {
+public class KeyItem(Texture2D keyTexture, Vector2 startPosition, CollisionManager collisionManager, ILevelManager levelManager) : IItem, IWorldPickup {
   // add collision info
   public FacingDirection Direction { get; set; } = FacingDirection.Right;
   private Rectangle sourceRectangle = new(0, 1344, 21, 39); // CHANGE
   private Vector2 origin;
-  private Texture2D texture;
-  private ILevelManager levelManager;
-  private CollisionManager collisionManager;
-  public Vector2 Position { get; set; }
-  public bool IsCollected { get; set; }
+  public Vector2 Position { get; set; } = startPosition;
+  public bool IsCollected { get; set; } = false;
   public ItemCategory Category { get; } = ItemCategory.Consumable;
-
-  public KeyItem(Texture2D keyTexture, Vector2 startPosition, CollisionManager collisionManager, ILevelManager levelManager) {
-    this.collisionManager = collisionManager;
-    this.levelManager = levelManager;
-    Position = startPosition;
-    texture = keyTexture;
-    IsCollected = false;
-  }
 
   public void Draw(SpriteBatch spriteBatch) {
     if (!IsCollected) {
       origin = new Vector2(sourceRectangle.Width / 2, sourceRectangle.Height / 2);
 
       spriteBatch.Draw(
-        texture,
+        keyTexture,
         Position,
         sourceRectangle,
         Color.White,
