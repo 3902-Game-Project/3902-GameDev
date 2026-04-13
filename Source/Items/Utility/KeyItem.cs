3435eecef362ba19a04e1640a3de8733afc10804
agvandy <12vandy81@gmail.com>
2026-04-13 14:03:43 -0400

    fixed key source rect

--- Source/Items/Utility/KeyItem.cs
+++ Source/Items/Utility/KeyItem.cs
@@ -8,7 +8,7 @@ using Microsoft.Xna.Framework.Graphics;
 namespace GameProject.Items.Utility;
 
 public class KeyItem(Texture2D keyTexture, Vector2 startPosition, ILevelManager levelManager) : IItem, IWorldPickup {
-  private Rectangle sourceRectangle = new(0, 1344, 21, 39);
+  private Rectangle sourceRectangle = new(17, 448, 7, 13);
   private readonly ILevelManager levelManagers = levelManager;
 
   public FacingDirection Direction { get; set; } = FacingDirection.Right;
