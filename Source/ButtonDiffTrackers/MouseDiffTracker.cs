15fcf52b3edc36e09dc500def583f926d619df46
santosh-sch <258964949+santosh-sch@users.noreply.github.com>
2026-04-12 22:24:52 -0400

    create abstract mouse controller

---
+++ Source/ButtonDiffTrackers/MouseDiffTracker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameProject.ButtonDiffTrackers;
+
+public enum MouseButtons {
+  Left,
+  Middle,
+  Right,
+  XButton1,
+  XButton2,
+};
+
+internal class MouseDiffTracker : ButtonDiffTracker<MouseButtons> {
+  public void Update(MouseState mouseState) {
+    var pressedButtons = new List<MouseButtons>();
+
+    if (mouseState.LeftButton == ButtonState.Pressed) {
+      pressedButtons.Add(MouseButtons.Left);
+    }
+
+    if (mouseState.MiddleButton == ButtonState.Pressed) {
+      pressedButtons.Add(MouseButtons.Middle);
+    }
+
+    if (mouseState.RightButton == ButtonState.Pressed) {
+      pressedButtons.Add(MouseButtons.Right);
+    }
+
+    if (mouseState.XButton1 == ButtonState.Pressed) {
+      pressedButtons.Add(MouseButtons.XButton1);
+    }
+
+    if (mouseState.XButton2 == ButtonState.Pressed) {
+      pressedButtons.Add(MouseButtons.XButton2);
+    }
+
+    UpdateButtonState([.. pressedButtons]);
+  }
+}
--- Source/Controllers/GameMouseController.cs
+++ Source/Controllers/GameMouseController.cs
@@ -1,27 +1,17 @@
-﻿using GameProject.Commands;
+﻿using System.Collections.Generic;
+using GameProject.ButtonDiffTrackers;
+using GameProject.Commands;
 using GameProject.Interfaces;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
 
 namespace GameProject.Controllers;
 
-internal class GameMouseController(Game1 game) : IController {
-  private MouseState prevMouseState = new();
-  private MouseState mouseState = new();
+internal class GameMouseController(Game1 game) : AMouseController {
+  protected override Dictionary<MouseButtons, ICommand> PressedMappings { get; } = new() {
+    { MouseButtons.Right, new PreviousLevelCommand(game.StateGame.LevelManager) },
+    { MouseButtons.Left, new NextLevelCommand(game.StateGame.LevelManager) },
+  };
 
-  private readonly ICommand prevLevelCommand = new PreviousLevelCommand(game.StateGame.LevelManager);
-  private readonly ICommand nextLevelCommand = new NextLevelCommand(game.StateGame.LevelManager);
+  protected override Dictionary<MouseButtons, ICommand> DownMappings { get; } = [];
 
-  public void Update(GameTime gameTime) {
-    prevMouseState = mouseState;
-    mouseState = Mouse.GetState();
-
-    if (mouseState.LeftButton == ButtonState.Pressed && prevMouseState.LeftButton == ButtonState.Released) {
-      nextLevelCommand.Execute();
-    }
-
-    if (mouseState.RightButton == ButtonState.Pressed && prevMouseState.RightButton == ButtonState.Released) {
-      prevLevelCommand.Execute();
-    }
-  }
+  protected override Dictionary<MouseButtons, ICommand> ReleasedMappings { get; } = [];
 }
