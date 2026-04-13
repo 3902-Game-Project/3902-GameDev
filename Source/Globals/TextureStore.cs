b3b6f9e97b1df012f91efbf360b4608c241e9c1d
santosh-sch <258964949+santosh-sch@users.noreply.github.com>
2026-04-12 20:46:53 -0400

    shorten name of health bar texture

--- Source/Globals/TextureStore.cs
+++ Source/Globals/TextureStore.cs
@@ -5,12 +5,12 @@ namespace GameProject.Globals;
 
 public static class TextureStore {
   public static Texture2D WhitePixel { get; private set; }
-  public static Texture2D HealthBarTexture { get; private set; }
+  public static Texture2D HealthBar { get; private set; }
 
   public static void Initialize() { }
 
   public static void LoadContent(ContentManager contentManager) {
     WhitePixel = contentManager.Load<Texture2D>("Misc/WhitePixel");
-    HealthBarTexture = contentManager.Load<Texture2D>("Misc/blood_red_bar");
+    HealthBar = contentManager.Load<Texture2D>("Misc/blood_red_bar");
   }
 }
