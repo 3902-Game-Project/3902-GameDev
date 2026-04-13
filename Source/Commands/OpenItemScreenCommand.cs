203db04edc89674f283b6a452056852e1c1d43f6
santosh-sch <258964949+santosh-sch@users.noreply.github.com>
2026-04-13 14:48:13 -0400

    use changestate for itemscreen

--- Source/Commands/OpenItemScreenCommand.cs
+++ Source/Commands/OpenItemScreenCommand.cs
@@ -4,6 +4,7 @@ namespace GameProject.Commands;
 
 public class OpenItemScreenCommand(Game1 game) : ICommand {
   public void Execute() {
-    game.ChangeStateWithoutFading(game.StateItemScreen);
+    // TODO Change this funciton to ChangeStateWithoutFading
+    game.ChangeState(game.StateItemScreen);
   }
 }
