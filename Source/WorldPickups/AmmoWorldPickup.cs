39c3e6bc0b72083a08f1fc6fb471b1894f36556d
emmettluzar <emmettluzar@gmail.com>
2026-04-14 19:30:03 -0400

    Refactored AmmoWorldPickup to support dynamic tinting and variable amounts

--- Source/WorldPickups/AmmoWorldPickup.cs
+++ Source/WorldPickups/AmmoWorldPickup.cs
@@ -1,29 +1,29 @@
 using GameProject.Collisions;
+using GameProject.Items;
+using GameProject.PlayerSpace;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace GameProject.WorldPickups;
 
-internal class AmmoWorldPickup(Texture2D texture, Vector2 position) : BaseWorldPickup(position), ICollidable {
+internal class AmmoWorldPickup(Texture2D texture, Vector2 position, AmmoType type, int amount) : BaseWorldPickup(position), ICollidable {
+  public AmmoType Type { get; } = type;
+  public int Amount { get; } = amount;
+
   private Rectangle sourceRectangle = new(9, 15, 12, 11);
   private Vector2 origin;
   private readonly float scale = 2f;
 
   public override void Draw(SpriteBatch spriteBatch) {
     origin = new Vector2(sourceRectangle.Width / 2, sourceRectangle.Height / 2);
-
-    spriteBatch.Draw(
-      texture,
-      Position,
-      sourceRectangle,
-      Color.White,
-      0f,
-      origin,
-      scale,
-      SpriteEffects.None,
-      0f
-    );
+    // Dynamic tinting using existing sprites
+    Color tintColor = Type == AmmoType.Light ? Color.Yellow : (Type == AmmoType.Heavy ? Color.Gray : Color.Red);
+    spriteBatch.Draw(texture, Position, sourceRectangle, tintColor, 0f, origin, scale, SpriteEffects.None, 0f);
   }
 
   public override void Update(GameTime gameTime) { }
+
+  public void OnPickup(Player player) {
+    player.Inventory.Ammo[Type] += Amount;
+  }
 }
