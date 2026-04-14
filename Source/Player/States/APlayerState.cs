719e8eb2312373c435e731e26e98d57e1e272d1f
santosh-sch <258964949+santosh-sch@users.noreply.github.com>
2026-04-13 23:13:29 -0400

    fix playerstate capture warnings

--- Source/Player/States/APlayerState.cs
+++ Source/Player/States/APlayerState.cs
@@ -6,6 +6,8 @@ using Microsoft.Xna.Framework.Graphics;
 namespace GameProject.PlayerSpace.States;
 
 internal abstract class APlayerState(Player player) : IPlayerState {
+  public Player Player { get; } = player;
+
   public abstract void MoveDown();
   public abstract void MoveLeft();
   public abstract void MoveRight();
@@ -14,11 +16,11 @@ internal abstract class APlayerState(Player player) : IPlayerState {
   public abstract void UseKey(UseType useType);
 
   public virtual void TakeDamage(int amount) {
-    if (!player.IsInvincible) {
-      player.Health -= amount;
-      player.InvincibilityTimer = Player.INVINCIBILITY_DURATION;
-      if (player.Health <= 0) {
-        player.Health = 0;
+    if (!Player.IsInvincible) {
+      Player.Health -= amount;
+      Player.InvincibilityTimer = Player.INVINCIBILITY_DURATION;
+      if (Player.Health <= 0) {
+        Player.Health = 0;
         Die();
       }
       SoundManager.Instance.Play(SoundID.PlayerHurt);
@@ -26,7 +28,7 @@ internal abstract class APlayerState(Player player) : IPlayerState {
   }
 
   public virtual void Die() {
-    player.State = player.DeadState;
+    Player.State = Player.DeadState;
   }
 
   public abstract void Update(GameTime gameTime);
