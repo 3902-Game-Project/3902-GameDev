82b7cb8de995e6435273034fed8016c34a62860a
santosh-sch <258964949+santosh-sch@users.noreply.github.com>
2026-04-25 17:55:36 -0400

    update formatting of rendertargettracker dispose

--- Source/Misc/RenderTargetTracker.cs
+++ Source/Misc/RenderTargetTracker.cs
@@ -21,26 +21,27 @@ internal class RenderTargetTracker(GraphicsDevice graphicsDevice) {
     protected virtual void Dispose(bool disposing) {
       if (!disposedValue) {
         if (disposing) {
-          // TODO: dispose managed state (managed objects)
+          // C# Docs: Dispose managed state (managed objects)
+          /* no managed state to dispose, if i understand managed state correctly */
         }
 
-        // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-        // TODO: set large fields to null
+        // C# Docs: Free unmanaged resources (unmanaged objects) and override finalizer
+        // C# Docs: Set large fields to null
         renderTargets.Pop();
         graphicsDevice.SetRenderTarget(renderTargets.Count > 0 ? renderTargets.Peek() : null);
         disposedValue = true;
       }
     }
 
-    // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    // // C# Docs: Override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
     // ~RenderTargetDisposer()
     // {
-    //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    //     // C# Docs: Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
     //     Dispose(disposing: false);
     // }
 
     public void Dispose() {
-      // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+      // C# Docs: Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
       Dispose(disposing: true);
       GC.SuppressFinalize(this);
     }
