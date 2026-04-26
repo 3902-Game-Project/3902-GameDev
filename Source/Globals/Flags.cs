454ba8b800fd9031a3abcfb6bc5b7362d2af5325
santosh-sch <258964949+santosh-sch@users.noreply.github.com>
2026-04-26 18:45:10 -0400

    reenable vignette

--- Source/Globals/Flags.cs
+++ Source/Globals/Flags.cs
@@ -3,7 +3,7 @@
 internal static class Flags {
   /* compile-time tweakables */
   public static readonly bool StartInDebugLevel = false;
-  public static readonly bool EnableVignette = false;
+  public static readonly bool EnableVignette = true;
 
   /* runtime tweakables */
   public static bool HaltAllUpdates { get; set; } = false;
