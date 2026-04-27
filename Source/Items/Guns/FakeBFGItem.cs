0f09500d89eda29af6d6aac6619a22c26d58adba
santosh-sch <258964949+santosh-sch@users.noreply.github.com>
2026-04-27 16:25:15 -0400

    tweak formatting, code redundancy

--- Source/Items/Guns/FakeBFGItem.cs
+++ Source/Items/Guns/FakeBFGItem.cs
@@ -9,22 +9,22 @@ using Microsoft.Xna.Framework.Graphics;
 namespace GameProject.Items;
 
 internal class FakeBFGItem : ABaseGun {
-  private readonly ILevelManager myLevelManager;
+  private readonly ILevelManager levelManager;
 
   public FakeBFGItem(Texture2D texture, Vector2 startPosition, Player player, ILevelManager levelManager, GunStats stats) :
     base(texture, startPosition, player, levelManager, stats) {
     Category = ItemCategory.Primary;
     sourceRectangle = new(0, 0, 40, 20);
-    bulletSpawnOffset = new Vector2(sourceRectangle.Width / 2f, 0) * scale;
+    bulletSpawnOffset = new Vector2(sourceRectangle.Width / 2.0f, 0.0f) * scale;
     fireMode = new SemiAutoFire(this.stats);
-    myLevelManager = levelManager;
+    this.levelManager = levelManager;
   }
 
   public override void Use(UseType useType) {
     if (EquipTimer > 0) return;
 
-    if (fireMode != null && fireMode.CanFire(useType)) {
-      myLevelManager.CurrentLevel.ProjectileManager.Add(ProjectileFactory.Instance.CreateBang(this));
+    if (fireMode.CanFire(useType)) {
+      levelManager.CurrentLevel.ProjectileManager.Add(ProjectileFactory.Instance.CreateBang(this));
 
       SoundManager.Instance.Play(SoundID.GunshotDefault);
     }
