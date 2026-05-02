85dd9eb1be0e903a6bd6ed70088f4b6f6b4f7f70
santosh-sch <258964949+santosh-sch@users.noreply.github.com>
2026-05-02 19:53:33 -0400

    move commands

--- Source/Commands/Misc/LoadGameCommand.cs
+++
@@ -1,9 +0,0 @@
-using GameProject.SaveLoad;
-
-namespace GameProject.Commands;
-
-internal class LoadGameCommand(Game1 game) : IGPCommand {
-  public void Execute() {
-    SaveLoadManager.LoadGame(game.StateGame);
-  }
-}
