1a8a323d97f76df0f5da717af2699b9a9a98e5d8
santosh-sch <258964949+santosh-sch@users.noreply.github.com>
2026-05-08 18:40:43 -0400

    move level loader cell entries dict
    details: move level loader cell entries dict to separate file

--- Source/Level/Loader/LevelLoader.cs
+++ Source/Level/Loader/LevelLoader.cs
@@ -5,9 +5,7 @@ using System.Text.RegularExpressions;
 using GameProject.Blocks;
 using GameProject.Commands;
 using GameProject.Enemies;
-using GameProject.Factories;
 using GameProject.Globals;
-using GameProject.Items;
 using GameProject.PlayerSpace;
 using GameProject.WorldPickups;
 using Microsoft.Xna.Framework;
@@ -28,54 +26,6 @@ internal partial class LevelLoader {
   public static readonly float PLAYER_TOP_POS_AFTER_TELEPORT = Constants.BASE_BLOCK_WIDTH * 1.5f;
   public static readonly float PLAYER_RIGHT_POS_AFTER_TELEPORT = Constants.LEVEL_WIDTH - Constants.BASE_BLOCK_WIDTH * 1.5f;
   public static readonly float PLAYER_BOTTOM_POS_AFTER_TELEPORT = Constants.LEVEL_HEIGHT - Constants.BASE_BLOCK_WIDTH * 1.5f;
-
-  private static readonly Dictionary<string, CellEntryParseFunc> CELL_ENTRY_FUNCS = new() {
-    { "", LevelLoaderCreatorFuncs.CreateEmptyCreator() }, /* empty, do nothing */
-    { "0", LevelLoaderCreatorFuncs.CreateEmptyCreator() }, /* empty, do nothing */
-    { "1-0", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateLogBlockSprite) }, /* log: wall */
-    { "1-1", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateLogCornerBlockSprite) }, /* log: corner */
-    { "2", LevelLoaderCreatorFuncs.CreateCollidableLockableDoorBlockCreator(BlockFactory.CreateSmallDoorBlockSprite) }, /* small door */
-    { "3", LevelLoaderCreatorFuncs.CreatePlayerPositionSetter() }, /* player position */
-    { "4", LevelLoaderCreatorFuncs.CreateEnemyCreator(EnemyFactory.Instance.CreateSnakeSprite) }, /* snake */
-    { "5", LevelLoaderCreatorFuncs.CreateNonCollidableBlockCreator(BlockFactory.CreateSandBlockSprite) }, /* sand */
-    { "6", LevelLoaderCreatorFuncs.CreateNonCollidableBlockCreator(BlockFactory.CreateRedSandBlockSprite) }, /* red sand */
-    { "7", LevelLoaderCreatorFuncs.CreateNonCollidableBlockCreator(BlockFactory.CreateWoodPlankBlockSprite) }, /* wood plank */
-    { "8-0", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateRockBlockSprite) }, /* rock: wall */
-    { "8-1", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateRockCornerBlockSprite) }, /* rock: corner */
-    /* 8-2: omitted */
-    { "8-3", LevelLoaderCreatorFuncs.CreateCollidableHoleBlockCreator(BlockFactory.CreateRockHoleBlockSprite) }, /* rock: hole to other room */
-    { "9", LevelLoaderCreatorFuncs.CreateFiringItemEnemyCreator(EnemyFactory.Instance.CreateShotgunnerSprite) }, /* shotgunner */
-    { "10", LevelLoaderCreatorFuncs.CreateEnemyCreator(EnemyFactory.Instance.CreateBatSprite) }, /* bat */
-    { "11", LevelLoaderCreatorFuncs.CreateFiringItemEnemyCreator(EnemyFactory.Instance.CreateRiflemanSprite) }, /* rifleman */
-    { "12", LevelLoaderCreatorFuncs.CreateEnemyCreator(EnemyFactory.Instance.CreateTumbleweedSprite) }, /* tumbleweed */
-    { "13", LevelLoaderCreatorFuncs.CreateEnemyCreator(EnemyFactory.Instance.CreateCactusSprite) }, /* cactus */
-    { "14", LevelLoaderCreatorFuncs.CreateGunItemCreator(ItemFactory.Instance.CreateRevolver) }, /* revolver */
-    { "15", LevelLoaderCreatorFuncs.CreateGunItemCreator(ItemFactory.Instance.CreateRifle) }, /* rifle */
-    { "16", LevelLoaderCreatorFuncs.CreateGunItemCreator(ItemFactory.Instance.CreateShotgun) }, /* shotgun */
-    { "17", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateBarrelBlockSprite) }, /* barrel */
-    { "18", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateBarShelfBlockSprite) }, /* bar shelf */
-    { "19", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateShelfBlockSprite) }, /* shelf */
-    { "20", LevelLoaderCreatorFuncs.CreateCollidableVaultDoorBlockCreator(BlockFactory.CreateVaultDoorBlockSprite) }, /* vault door */
-    { "21", LevelLoaderCreatorFuncs.CreateKeyItemCreator(ItemFactory.CreateKey) }, /* key item */
-    { "22", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateFirePitBlockSprite) }, /* fire pit */
-    { "23", LevelLoaderCreatorFuncs.CreateCollidablePlayerBlockCreator(BlockFactory.CreateFireBlockSprite) }, /* fire */
-    /* 24: omitted */
-    { "25", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateMudBlockSprite) }, /* mud */
-    { "26", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateCrateBlockSprite) }, /* crate */
-    { "27", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateStoolBlockSprite) }, /* stool */
-    { "28", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateTableBlockSprite) }, /* table */
-    { "29", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateStatueBlockSprite) }, /* statue */
-    { "30", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateWindowBlockSprite) }, /* window */
-    { "31", LevelLoaderCreatorFuncs.CreateCollidableLockableDoorBlockCreator(BlockFactory.CreateSlattedDoorSprite) }, /* slatted door */
-    { "32", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateTreasureBlockSprite) }, /* treasure block */
-    { "33", LevelLoaderCreatorFuncs.CreateAmmoItemCreator(WorldPickupFactory.CreateAmmo) }, /* ammo item */
-    { "34", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateBankShelfBlockSprite) }, /* bank shelf */
-    { "35", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateTellersDeskBlockSprite) }, /* tellers desk */
-    { "36", LevelLoaderCreatorFuncs.CreatePlayerItemCreator(ItemFactory.Instance.CreateHealthPotion) }, /* health potion item */
-    { "37", LevelLoaderCreatorFuncs.CreatePlayerItemCreator(ItemFactory.Instance.CreateInvincibilityPotion) }, /* invincibility potion item */
-    { "38", LevelLoaderCreatorFuncs.CreatePlayerItemCreator(ItemFactory.Instance.CreateInfiniteAmmoPotion) }, /* infinite ammo potion item */
-    { "39", LevelLoaderCreatorFuncs.CreateFiringEnemyCreator(EnemyFactory.CreateBossSprite) }, /* boss */
-  };
 
   private static void ParseSingleFlag(LevelFlags flags, string flag) {
     switch (flag) {
@@ -132,7 +82,7 @@ internal partial class LevelLoader {
     List<IWorldPickup> pickups,
     ref Vector2? playerPositionNullable
   ) {
-    if (CELL_ENTRY_FUNCS.TryGetValue(type, out var EntryParseFunc)) {
+    if (LevelLoaderCellEntries.CELL_ENTRY_FUNCS.TryGetValue(type, out var EntryParseFunc)) {
       EntryParseFunc(
         player,
         levelManager,
---
+++ Source/Level/Loader/LevelLoaderCellEntries.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using GameProject.Blocks;
+using GameProject.Enemies;
+using GameProject.Factories;
+using GameProject.Items;
+
+namespace GameProject.Level.Loader;
+
+internal class LevelLoaderCellEntries {
+  public static readonly Dictionary<string, CellEntryParseFunc> CELL_ENTRY_FUNCS = new() {
+    { "", LevelLoaderCreatorFuncs.CreateEmptyCreator() }, /* empty, do nothing */
+    { "0", LevelLoaderCreatorFuncs.CreateEmptyCreator() }, /* empty, do nothing */
+    { "1-0", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateLogBlockSprite) }, /* log: wall */
+    { "1-1", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateLogCornerBlockSprite) }, /* log: corner */
+    { "2", LevelLoaderCreatorFuncs.CreateCollidableLockableDoorBlockCreator(BlockFactory.CreateSmallDoorBlockSprite) }, /* small door */
+    { "3", LevelLoaderCreatorFuncs.CreatePlayerPositionSetter() }, /* player position */
+    { "4", LevelLoaderCreatorFuncs.CreateEnemyCreator(EnemyFactory.Instance.CreateSnakeSprite) }, /* snake */
+    { "5", LevelLoaderCreatorFuncs.CreateNonCollidableBlockCreator(BlockFactory.CreateSandBlockSprite) }, /* sand */
+    { "6", LevelLoaderCreatorFuncs.CreateNonCollidableBlockCreator(BlockFactory.CreateRedSandBlockSprite) }, /* red sand */
+    { "7", LevelLoaderCreatorFuncs.CreateNonCollidableBlockCreator(BlockFactory.CreateWoodPlankBlockSprite) }, /* wood plank */
+    { "8-0", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateRockBlockSprite) }, /* rock: wall */
+    { "8-1", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateRockCornerBlockSprite) }, /* rock: corner */
+    /* 8-2: omitted */
+    { "8-3", LevelLoaderCreatorFuncs.CreateCollidableHoleBlockCreator(BlockFactory.CreateRockHoleBlockSprite) }, /* rock: hole to other room */
+    { "9", LevelLoaderCreatorFuncs.CreateFiringItemEnemyCreator(EnemyFactory.Instance.CreateShotgunnerSprite) }, /* shotgunner */
+    { "10", LevelLoaderCreatorFuncs.CreateEnemyCreator(EnemyFactory.Instance.CreateBatSprite) }, /* bat */
+    { "11", LevelLoaderCreatorFuncs.CreateFiringItemEnemyCreator(EnemyFactory.Instance.CreateRiflemanSprite) }, /* rifleman */
+    { "12", LevelLoaderCreatorFuncs.CreateEnemyCreator(EnemyFactory.Instance.CreateTumbleweedSprite) }, /* tumbleweed */
+    { "13", LevelLoaderCreatorFuncs.CreateEnemyCreator(EnemyFactory.Instance.CreateCactusSprite) }, /* cactus */
+    { "14", LevelLoaderCreatorFuncs.CreateGunItemCreator(ItemFactory.Instance.CreateRevolver) }, /* revolver */
+    { "15", LevelLoaderCreatorFuncs.CreateGunItemCreator(ItemFactory.Instance.CreateRifle) }, /* rifle */
+    { "16", LevelLoaderCreatorFuncs.CreateGunItemCreator(ItemFactory.Instance.CreateShotgun) }, /* shotgun */
+    { "17", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateBarrelBlockSprite) }, /* barrel */
+    { "18", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateBarShelfBlockSprite) }, /* bar shelf */
+    { "19", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateShelfBlockSprite) }, /* shelf */
+    { "20", LevelLoaderCreatorFuncs.CreateCollidableVaultDoorBlockCreator(BlockFactory.CreateVaultDoorBlockSprite) }, /* vault door */
+    { "21", LevelLoaderCreatorFuncs.CreateKeyItemCreator(ItemFactory.CreateKey) }, /* key item */
+    { "22", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateFirePitBlockSprite) }, /* fire pit */
+    { "23", LevelLoaderCreatorFuncs.CreateCollidablePlayerBlockCreator(BlockFactory.CreateFireBlockSprite) }, /* fire */
+    /* 24: omitted */
+    { "25", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateMudBlockSprite) }, /* mud */
+    { "26", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateCrateBlockSprite) }, /* crate */
+    { "27", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateStoolBlockSprite) }, /* stool */
+    { "28", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateTableBlockSprite) }, /* table */
+    { "29", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateStatueBlockSprite) }, /* statue */
+    { "30", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateWindowBlockSprite) }, /* window */
+    { "31", LevelLoaderCreatorFuncs.CreateCollidableLockableDoorBlockCreator(BlockFactory.CreateSlattedDoorSprite) }, /* slatted door */
+    { "32", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateTreasureBlockSprite) }, /* treasure block */
+    { "33", LevelLoaderCreatorFuncs.CreateAmmoItemCreator(WorldPickupFactory.CreateAmmo) }, /* ammo item */
+    { "34", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateBankShelfBlockSprite) }, /* bank shelf */
+    { "35", LevelLoaderCreatorFuncs.CreateCollidableBlockCreator(BlockFactory.CreateTellersDeskBlockSprite) }, /* tellers desk */
+    { "36", LevelLoaderCreatorFuncs.CreatePlayerItemCreator(ItemFactory.Instance.CreateHealthPotion) }, /* health potion item */
+    { "37", LevelLoaderCreatorFuncs.CreatePlayerItemCreator(ItemFactory.Instance.CreateInvincibilityPotion) }, /* invincibility potion item */
+    { "38", LevelLoaderCreatorFuncs.CreatePlayerItemCreator(ItemFactory.Instance.CreateInfiniteAmmoPotion) }, /* infinite ammo potion item */
+    { "39", LevelLoaderCreatorFuncs.CreateFiringEnemyCreator(EnemyFactory.CreateBossSprite) }, /* boss */
+  };
+}
